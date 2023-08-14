7b60f588056cafbf66ffbf04376ddc13f35cc22b
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-08-14 10:33:31 +0900

    niches#index、niches#showの簡易実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: "niches#index"
   devise_for :users, controllers: {
     registrations: 'custom_devise/registrations' # カスタムのコントローラーを使用
   }
-  resources :niches do
+  resources :niches, path: '/', as: :niche do
     resources :posts
   end
-  # カスタムの show アクションへのルーティング
+
   get 'user_profile', to: 'custom_devise/registrations#show', as: :user_profile
 end
