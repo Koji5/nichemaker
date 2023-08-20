6801db6fbe138a1f4c11cb7c79a9970ca46f0d92
Koji5 <76521976+Koji5@users.noreply.github.com>
2023-08-20 21:28:44 +0900

    投稿機能_保存まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,22 +6,60 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @niche = Niche.find(params[:niche_id])
-    @niche_progress_groups = NicheProgressGroup.where(niche_id: params[:niche_id]).order(:name)
-    @niche_progress_tasks = NicheProgressTask.where(niche_progress_group_id: @niche_progress_groups.first.id).order(:name)
-    @niche_parameters = NicheParameter.where(niche_id: params[:niche_id]).order(:name)
-    @post_parameter = PostParameter.new
+    get_data
+  end
+
+  def create
+    binding.pry
+    @post = Post.new(post_params)
+    @niche = Niche.find(params[:niche_id])
+    if @post.save
+      save_post_parameters
+      save_progress_rates
+      redirect_to niche_post_path(@niche.id, @post)
+    else
+      get_data
+      render :new
+    end
   end
 
   def show
   end
 
   private
+
+  def get_data
+    @niche_progress_groups = NicheProgressGroup.where(niche_id: params[:niche_id]).order(:name)
+    @niche_progress_tasks = NicheProgressTask.where(niche_progress_group_id: @niche_progress_groups.first.id).order(:name)
+    @niche_parameters = NicheParameter.where(niche_id: params[:niche_id]).order(:name)
+    @post_parameter = PostParameter.new
+  end
+
+  def save_post_parameters
+    post_parameter_params = params[:post_parameters]
+    post_parameter_params.each do |post_parameter_param|
+      post_parameter = PostParameter.new(post_parameter_param.permit(:niche_parameter_id, :value))
+      post_parameter.post_id = @post.id
+      post_parameter.save
+    end
+  end
+
+  def save_progress_rates
+    progress_rate = ProgressRate.new(progress_rate_params)
+    progress_rate.save
+  end
+
   def post_params
-#    params.require(:post).permit(
-#      :title, :content,
-#      progress_rate_attributes: [:attribute_name1, :attribute_name2],
-#      post_parameters_attributes: [:attribute_name3, :attribute_name4]
-#      , {images: []}
-#    )
+    params.permit(
+      :title,
+      :content,
+      :posted_at,
+      :niche_id,
+      {images: []}
+    ).merge(user_id: current_user.id)
+  end
+
+  def progress_rate_params
+    params.permit(:rate, :niche_progress_task_id).merge(post_id: @post.id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,5 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :niche
   has_many_attached :images
-  accepts_nested_attributes_for :post_parameters, :progress_rate
   validates :images, length: { minimum: 0, maximum: 20}
 end
